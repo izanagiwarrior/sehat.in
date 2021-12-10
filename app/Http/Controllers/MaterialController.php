ea6445c102c7241615cfd284b8bb4df9ff9f356f
M. Faiz Triputra <m.faiztpaduhai@gmail.com>
2021-12-10 10:21:48 +0700

    progress

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Material;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use DataTables;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Auth;
 
 class MaterialController extends Controller
 {
@@ -12,9 +18,22 @@ class MaterialController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request,$id)
     {
-        //
+        if ($request->ajax()) {
+            $data = Material::where('id_product','=',$id)->get();
+            return DataTables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function ($data) {
+                    $button = '<a href="' . action('MaterialController@update_view', $data->id) . '" type="button" name="edit" id="' . $data->id . '" class="edit btn btn-primary btn-sm">Edit</a>';
+                    $button .= '&nbsp;&nbsp;&nbsp;<a data-toggle="confirmation" data-singleton="true" data-popout="true" href="' . action('MaterialController@delete', $data->id) . '" type="button" name="delete" id="' . $data->id . '" class="delete btn btn-danger btn-sm"' . "onclick='return'" . '>Delete</a>';
+                    return $button;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        return view('cms.material.material');
     }
 
     /**
@@ -22,9 +41,40 @@ class MaterialController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create_view()
     {
-        //
+        $category = Category::all();
+        $crafter = Auth::user();
+        return view('cms.material.create', compact('category', 'crafter'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create_process(Request $request)
+    {
+        $request->validate([
+            'category' => 'required',
+            'crafter' => 'required',
+            'title' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+            'foto' => 'required', 'mimes:jpg,jpeg,png',
+        ]);
+
+        $product = new Products();
+        $product->id_category = $request->category;
+        $product->crafter = $request->crafter;
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        $product->photo = Storage::disk('public')->put('product', $request->file('foto'));
+
+        $product->save();
+
+        return redirect()->route('material')->withSuccess('Product created successfully.');
     }
 
     /**
@@ -33,53 +83,58 @@ class MaterialController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function update_view($id)
     {
-        //
+        $data = Products::find($id);
+        $category = Category::all();
+        return view('cms.material.update', compact('data', 'category'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Material  $material
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Material $material)
+    public function update_process(Request $request, $id)
     {
-        //
-    }
+        $request->validate([
+            'category' => 'required',
+            'crafter' => 'required',
+            'title' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+            'foto' => 'mimes:jpg,jpeg,png',
+        ]);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Material  $material
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Material $material)
-    {
-        //
-    }
+        $product = Products::find($id);
+        $product->id_category = $request->category;
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->description = $request->description;
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Material  $material
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Material $material)
-    {
-        //
+        if (isset($request->foto)) {
+            $image_path = 'storage/' . $product->photo;
+            if (File::exists($image_path)) {
+                File::delete($image_path);
+            }
+            $product->photo = Storage::disk('public')->put('product', $request->file('foto'));
+        }
+
+        $product->save();
+
+        return redirect()->route('material')->withSuccess('Product updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Material  $material
+     * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Material $material)
+    public function delete($id)
     {
-        //
+        $product = Products::find($id);
+        $image_path = 'storage/' . $product->photo;
+        if (File::exists($image_path)) {
+            File::delete($image_path);
+        }
+        $product->delete();
+
+        return redirect()->route('material')->withSuccess('Product deleted successfully.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -35,31 +35,16 @@ Route::middleware('auth')->group(function () {
 
 
     Route::prefix('admin')->middleware('admin')->group(function () {
-        // Product
-        Route::prefix('product')->name('product')->group(function () {
-            Route::get('/', 'ProductsController@index')->name('');
-            Route::get('/create', 'ProductsController@create_view')->name('.create');
-            Route::post('/create', 'ProductsController@create_process')->name('.create.process');
-            Route::get('/update/{id}', 'ProductsController@update_view')->name('.update');
-            Route::post('/update/{id}', 'ProductsController@update_process')->name('.update.process');
-            Route::get('/delete/{id}', 'ProductsController@delete')->name('.delete');
-        });
 
-        // Categories
-        Route::prefix('category')->name('category')->group(function () {
-            Route::get('/', 'CategoryController@index')->name('');
-            Route::get('/create', 'CategoryController@create_view')->name('.create');
-            Route::post('/create', 'CategoryController@create_process')->name('.create.process');
-            Route::get('/update/{id}', 'CategoryController@update_view')->name('.update');
-            Route::post('/update/{id}', 'CategoryController@update_process')->name('.update.process');
-            Route::get('/delete/{id}', 'CategoryController@delete')->name('.delete');
-        });
+        // ========================= DEFAULT =========================
 
         // Store
         Route::prefix('profile')->name('profile')->group(function () {
             Route::get('/', 'ProfileController@index')->name('');
             Route::put('/', 'ProfileController@update')->name('.update');
         });
+
+        // ========================= MANAGEMENT =========================
 
         // Users
         Route::prefix('user')->name('user')->group(function () {
@@ -72,10 +57,60 @@ Route::middleware('auth')->group(function () {
             Route::get('/delete/{id}', 'UsersController@delete')->name('.delete');
         });
 
+        // Categories
+        Route::prefix('category')->name('category')->group(function () {
+            Route::get('/', 'CategoryController@index')->name('');
+            Route::get('/create', 'CategoryController@create_view')->name('.create');
+            Route::post('/create', 'CategoryController@create_process')->name('.create.process');
+            Route::get('/update/{id}', 'CategoryController@update_view')->name('.update');
+            Route::post('/update/{id}', 'CategoryController@update_process')->name('.update.process');
+            Route::get('/delete/{id}', 'CategoryController@delete')->name('.delete');
+        });
+
+        // Product
+        Route::prefix('product')->name('product')->group(function () {
+            Route::get('/', 'ProductsController@index')->name('');
+            Route::get('/create', 'ProductsController@create_view')->name('.create');
+            Route::post('/create', 'ProductsController@create_process')->name('.create.process');
+            Route::get('/update/{id}', 'ProductsController@update_view')->name('.update');
+            Route::post('/update/{id}', 'ProductsController@update_process')->name('.update.process');
+            Route::get('/delete/{id}', 'ProductsController@delete')->name('.delete');
+        });
+
+        // Materials
+        Route::prefix('material')->name('material')->group(function () {
+            Route::get('/{id_product}', 'MaterialController@index')->name('');
+            Route::get('/{id_product}/create', 'MaterialController@create_view')->name('.create');
+            Route::post('/{id_product}/create', 'MaterialController@create_process')->name('.create.process');
+            Route::get('/{id_product}/update/{id}', 'MaterialController@update_view')->name('.update');
+            Route::post('/{id_product}/update/{id}', 'MaterialController@update_process')->name('.update.process');
+            Route::get('/{id_product}/delete/{id}', 'MaterialController@delete')->name('.delete');
+        });
+
+        // Cart
+        Route::prefix('cart')->name('cart')->group(function () {
+            Route::get('/', 'CartController@index')->name('');
+            Route::get('/create', 'CartController@create_view')->name('.create');
+            Route::post('/create', 'CartController@create_process')->name('.create.process');
+            Route::get('/delete/{id}', 'CartController@delete')->name('.delete');
+        });
+
+        // Checkout
+        Route::prefix('checkout')->name('checkout')->group(function () {
+            Route::get('/', 'CheckoutController@index')->name('');
+            Route::get('/create', 'CheckoutController@create_view')->name('.create');
+            Route::post('/create', 'CheckoutController@create_process')->name('.create.process');
+            Route::get('/delete/{id}', 'CheckoutController@delete')->name('.delete');
+        });
+
+        // Order
+        Route::prefix('order')->name('order')->group(function () {
+            Route::get('/', 'OrderController@index')->name('');
+            Route::get('/create', 'OrderController@create_view')->name('.create');
+            Route::post('/create', 'OrderController@create_process')->name('.create.process');
+            Route::get('/delete/{id}', 'OrderController@delete')->name('.delete');
+        });
 
 
     });
-
-
-
 });
